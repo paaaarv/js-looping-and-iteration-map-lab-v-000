dc9de546474807a57787cf5a45fea4e694cd6ae4
Parv Kumar <parvathikumar92@gmail.com>
2017-10-20 17:05:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,8 +10,5 @@ function lowerCaseDrivers(drivers){
 function nameToAttributes(drivers){
   return drivers.map(function(driver){
     return Object.assign({}, {firstName: driver.split(" ")[0], lastName: driver.split(" ")[1]})
-  }
-
-)
-
+  })
 }
