ee2898c2471e622c9559727d97de9cf2e379e393
Parv Kumar <parvathikumar92@gmail.com>
2017-10-20 16:34:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,10 +3,7 @@
 function lowerCaseDrivers(drivers){
 
   return lowerCase = drivers.map(function(driver){
-    let newArr = []
-    for (const element of drivers){
-      newArr.push(element.toLowerCase())
-    })
-    return newArr;
-};
+
+
+  };
 };
